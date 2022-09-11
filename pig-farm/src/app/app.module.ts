6f39c5c8053c863fe7abb05a3d4a6f19459d72b4
HOANG SANG <hoangsang97@gmail.com>
2022-09-11 15:35:00 +0700

    sangNH up security

--- pig-farm/src/app/app.module.ts
+++ pig-farm/src/app/app.module.ts
@@ -5,6 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
+import {SecurityModule} from './security/security.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {ToastrModule} from 'ngx-toastr';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    SecurityModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
