cb4f71b8df1ee5ba93b31c9c1940c2ac20d66e22
tthuan1 <tranthuan1809@gmail.com>
2022-09-12 17:00:36 +0700

    ThuanT up date 12/09

---
+++ pig-farm/src/app/treatment/list-treatment/list-treatment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-treatment',
+  templateUrl: './list-treatment.component.html',
+  styleUrls: ['./list-treatment.component.css']
+})
+export class ListTreatmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
