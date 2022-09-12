84a9c7a58e42ffb0d14e34d0e05194dd7c2e3872
ndphucc <ntamque@gmail.com>
2022-09-12 16:44:53 +0700

    PhucND up pigsty

---
+++ pig-farm/src/app/pigsty/list-pigsty/list-pigsty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pigsty',
+  templateUrl: './list-pigsty.component.html',
+  styleUrls: ['./list-pigsty.component.css']
+})
+export class ListPigstyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ pig-farm/src/app/pigsty/pigsty.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListPigstyComponent } from './list-pigsty/list-pigsty.component';
+
+
+@NgModule({
+  declarations: [ListPigstyComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PigstyModule { }
