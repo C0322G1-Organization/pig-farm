f4e3daa53e9631a8bc5fd1b16289788370c64d5a
thanhtri3010 <thanhtri_pham2010@yahoo.com>
2022-09-13 15:43:20 +0700

    TriPT update 13/09

--- pig-farm/src/app/contact/service/contact.service.ts
+++ pig-farm/src/app/contact/service/contact.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Contact} from '../model/contact';
+import {environment} from '../../../environments/environment';
+
+const URL_CONTACT = `${environment.apiUrl + '/api/contact'}`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  private URL_CONTACT = 'http://localhost:8080/api/contact';
 
   constructor(private httpClient: HttpClient) {
   }
@@ -18,7 +20,7 @@ export class ContactService {
    * Function: getAll Contact
    */
   getAllContact(page: number, nameSearch: string): Observable<Contact[]> {
-    return this.httpClient.get<Contact[]>(this.URL_CONTACT + '/page?page=' + page + '&nameSearch=' + nameSearch);
+    return this.httpClient.get<Contact[]>(URL_CONTACT + '/page?page=' + page + '&nameSearch=' + nameSearch);
   }
 
   /**
@@ -27,12 +29,12 @@ export class ContactService {
    * Function: findById
    */
   getContactById(id: number): Observable<Contact> {
-    return this.httpClient.get<Contact>(this.URL_CONTACT + `/${id}`);
+    return this.httpClient.get<Contact>(URL_CONTACT + `/${id}`);
   }
 
   deleteContact(ids: number[]): Observable<any> {
     const data = {id: ids};
-    const url = this.URL_CONTACT + '/delete';
+    const url = URL_CONTACT + '/delete';
     return this.httpClient.post<any>(url, data);
   }
 
