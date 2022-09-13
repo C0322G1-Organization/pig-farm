e9b3399fe499edd14437a1fba6d0decab1018c64
hoangsang97 <hoangsang97@gmail.com>
2022-09-13 15:54:34 +0700

    ThuanT up code list and delete treatment

--- pig-farm/src/app/service/pig-service.ts
+++ pig-farm/src/app/service/pig-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
-const API_URL = `${environment.API_URL}`;
+const API_URL = `${environment.apiUrl}`;
 @Injectable({
   providedIn: 'root'
 })
