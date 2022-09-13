42aef77f5688e23da78bd59913c47da3167da266
tthuan1 <tranthuan1809@gmail.com>
2022-09-13 15:38:07 +0700

    thuan up code list  and delete treatment

--- pig-farm/src/app/service/treatment-service.ts
+++ pig-farm/src/app/service/treatment-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Treatment} from '../model/treatment';
-const API_URL = `${environment.API_URL}`;
+const API_URL = `${environment.apiUrl}`;
 import {Observable} from 'rxjs';
 @Injectable({
   providedIn: 'root'
