35e4359f7c4b12a3c7990a6920fc789662bc5b99
hoangsang97 <hoangsang97@gmail.com>
2022-09-13 19:53:07 +0700

    SangNH update vaccination list and create

--- pig-farm/src/app/vaccination/vaccination.module.ts
+++ pig-farm/src/app/vaccination/vaccination.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {VaccinationRoutingModule} from './vaccination-routing.module';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {VaccinationCreateComponent} from './vaccination-create/vaccination-create.component';
 import {HttpClientModule} from '@angular/common/http';
 import {VaccinationListComponent} from './vaccination-list/vaccination-list.component';
-
-
 
 @NgModule({
   declarations: [VaccinationCreateComponent, VaccinationListComponent],
@@ -15,6 +13,7 @@ import {VaccinationListComponent} from './vaccination-list/vaccination-list.comp
     VaccinationRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    FormsModule,
   ]
 })
 export class VaccinationModule { }
