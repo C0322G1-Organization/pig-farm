dd942e232c6ac11e0fe1cbc935e4ed6dfa5a145d
tranvanhai591989 <102501534+tranvanhai591989@users.noreply.github.com>
2022-09-13 15:20:43 +0700

    HaiTV up FE displaylist and notification-service

--- pig-farm/src/app/notification/notification.service.ts
+++ pig-farm/src/app/notification/notification.service.ts
@@ -1,33 +1,34 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Notification} from '../entity/notification';
 import {environment} from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
+
 const URL_NOTIFICATION = `${environment.apiUrl + '/api/v1/notification'}`;
+
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class NotificationService {
 
   constructor(private http: HttpClient) {
   }
 
-  getAllNotifications(page: number): Observable<Notification[]> {
-    return this.http.get<Notification[]>(URL_NOTIFICATION + '/page?page=' + page);
+  getAllNotifications(page: number, contentSearch: string): Observable<Notification[]> {
+    return this.http.get<Notification[]>(URL_NOTIFICATION + '/page?page=' + page + '&content=' + contentSearch);
   }
 
   deleteNotifications(ids: number[]): Observable<any> {
     const data = {id: ids};
     const url = URL_NOTIFICATION + '/delete';
     return this.http.post<any>(url, data);
-  }
-
-  searchByContent(content: any): Observable<Notification[]> {
-    const contentSearch: string = content.content;
-    return this.http.get<Notification[]>(URL_NOTIFICATION + '/page?content=' + contentSearch);
   }
 }
