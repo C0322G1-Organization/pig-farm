8d8c2bedf33b7be93311dc7bc06e05103bdf62c9
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-09-14 09:17:50 +0700

    DongLHP update export list.component.ts

--- pig-farm/src/app/app.module.ts
+++ pig-farm/src/app/app.module.ts
@@ -5,30 +5,26 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
-import { ListComponent } from './export_port/component/list/list.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BodyModule} from './body/body.module';
 import {ExportModule} from './export_port/router/export.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    ReactiveFormsModule,
     ExportModule,
-    ToastrModule.forRoot({
-      timeOut: 1000,
-      progressBar: true,
-      progressAnimation: 'increasing',
-      preventDuplicates: true
-    })
+    ToastrModule.forRoot(),
+    BodyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
