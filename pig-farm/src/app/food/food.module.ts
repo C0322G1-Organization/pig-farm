5be876d6715df60578ccc5e5233d692afda4f8b6
hungnv612 <hungdaubasg@gmail.com>
2022-09-14 16:58:41 +0700

    update food-HungNV

---
+++ pig-farm/src/app/food/food.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FoodRoutingModule } from './food-routing.module';
+import { FoodCreateComponent } from './food-create/food-create.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { FoodEditComponent } from './food-edit/food-edit.component';
+
+
+@NgModule({
+  declarations: [FoodCreateComponent, FoodEditComponent],
+    imports: [
+        CommonModule,
+        FoodRoutingModule,
+        ReactiveFormsModule
+    ]
+})
+export class FoodModule { }
