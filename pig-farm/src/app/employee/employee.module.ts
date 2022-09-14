2f2383094de782a7191007af02d96e059e7ea280
unknown <9challenger8@gmail.com>
2022-09-14 08:47:28 +0700

    HungNQ up frontend

--- pig-farm/src/app/employee/employee.module.ts
+++ pig-farm/src/app/employee/employee.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeeListComponent
+  ],
   imports: [
     CommonModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class EmployeeModule { }
