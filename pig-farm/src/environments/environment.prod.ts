c17b1290f493d2f1c938a3c7de111b9037e5d7a5
trungdc-ha <dangchitrungha@gmail.com>
2022-09-14 10:54:53 +0700

    TrungDC update file config environment.prod.ts

--- pig-farm/src/environments/environment.prod.ts
+++ pig-farm/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://trungdc.synology.me:5152/pig-farm-api'
+  apiUrl: 'https://trungdc.synology.me:5152/pig-farm-api',
+  firebaseConfig : {
+    apiKey: 'AIzaSyBYH6IrJ1z76jwUfO6bqSwug9keJDuGrOM',
+    authDomain: 'notification-1b988.firebaseapp.com',
+    databaseURL: 'https://notification-1b988-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'notification-1b988',
+    storageBucket: 'notification-1b988.appspot.com',
+    messagingSenderId: '482166598328',
+    appId: '1:482166598328:web:cd4a422766d3ca2056a24d',
+    measurementId: 'G-NPMC25R69V'
+  }
 };
