0ab9b75456e19e196ce7f6393009b6787a1df1fd
lethuandat <levanle5@gmail.com>
2022-09-15 00:52:03 +0700

    DatLT update code for list news

--- pig-farm/src/app/vaccination/vaccination-routing.module.ts
+++ pig-farm/src/app/vaccination/vaccination-routing.module.ts
@@ -1,8 +1,13 @@
 import {RouterModule, Routes} from '@angular/router';
 import {VaccinationCreateComponent} from './vaccination-create/vaccination-create.component';
 import {NgModule} from '@angular/core';
+import {VaccinationListComponent} from './vaccination-list/vaccination-list.component';
 
 const routes: Routes = [
+  {
+    path: 'vaccination',
+    component: VaccinationListComponent
+  },
   {
     path: 'api/vaccination/create',
     component: VaccinationCreateComponent
